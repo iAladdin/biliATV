2b15709a07e64e5be5ae7c046cf97eb8afd1d84b
aladdin <aladdin@ddex.io>
2018-10-28 02:14:05 +0800

    using hd resolution as default

--- TVML/application.js
+++ TVML/application.js
@@ -52,7 +52,73 @@ var window = this;
 * https://api.bilibili.com/x/web-interface/tag/top?pn=1&ps=30&callback=relate_video_callback&jsonp=jsonp&tid=4641922&_=1511098218419
 * */
 
+
+
 var displayErrorLock = false;
+var MD5 = function(d){result = M(V(Y(X(d),8*d.length)));return result.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_};
+function getStringFromURL(url) {
+    var request = new XMLHttpRequest;
+    url = encodeURI(url);
+    request.open("GET", url, false);
+    request.send();
+    return request.response;
+}
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36';
+const APPKEY = '85eb6835b0a1034e';
+const APPSEC = '2ad42749773c441109bdc0191257a664';
+
+function GetSign(params,appkey,AppSecret=None){
+    params['appkey']=appkey
+    let data = "";
+    paras = Object.keys(params);
+    paras.sort()
+    paras.forEach(para => {
+        if (data !== ""){
+            data += "&";
+        }
+        data += para + '=' + `${params[para]}`
+    });
+    if (!AppSecret){
+        return data;
+    }else{
+        return data+ MD5(data+AppSecret);
+    }
+}
+
+function getCid(aid){
+    const url = `http://www.bilibili.com/widget/getPageList?aid=${aid}`
+    const info = jsonParse(getStringFromURL(url));
+    return info[0];
+}
+function api_req(cid,quality){
+    let ts = Date.now();
+    // if not bangumi:
+        // let params_str = `cid=${cid}&player=1&quality=${quality}&ts=${ts}`;
+        const params_str = `appkey=84956560bc028eb7&cid=${cid}&otype=json&qn=${quality}&quality=${quality}&type=`;
+        console.log(params_str,'94aba54af9065f71de72f5508f1cd42e');
+        const chksum = MD5(params_str+'94aba54af9065f71de72f5508f1cd42e');
+        api_url = 'http://interface.bilibili.com/v2/playurl?' + params_str + '&sign=' + chksum;
+        console.log(api_url);
+        const data = jsonParse(getStringFromURL(api_url));
+        console.log(data);
+        return data;
+    // else:
+    //     mod = 'movie' if bangumi_movie else 'bangumi'
+    //     params_str = 'cid={}&module={}&player=1&quality={}&ts={}'.format(cid, mod, quality, ts)
+    //     chksum = hashlib.md5(bytes(params_str+self.SEC2, 'utf8')).hexdigest()
+    //     api_url = self.bangumi_api_url + params_str + '&sign=' + chksum
+
+    // xml_str = get_content(api_url, headers={'referer': self.url, 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'})
+    // return xml_str
+}
+function getBilibiliURL(aid,pid,cb) {
+    const cid_args = getCid(aid);
+    console.log(cid_args);
+    const data = api_req(cid_args.cid,116);
+    data.name = cid_args.pagename;
+    data.aid = aid;
+    cb(data);
+}
 
 // 显示错误信息
 function displayError(title="", info="") {
@@ -1210,17 +1276,16 @@ function testView(testInfo){
 }
 function playDMAV(id=30621030,page=2,data=null) {
     var _play = function (data,page) {
-        const dataObject = jsonParse(data);
-        let part = dataObject.data;
+        let part = data;
         console.log('[_play],part',part);
         if(part){
             // let timeMap = [];
             var video_url = '';
-            let videos = part.slice();
-            videos = videos.filter( item =>{
-                return item.type === 'single'
-            });
-            video_url = videos[videos.length-1].url;
+            let url = part.durl[0].url;
+            // videos = videos.filter( item =>{
+            //     return item.type === 'single'
+            // });
+            video_url = url;
 
             console.log('[playDMAV] data',data,video_url);
             // if(part.playData.durl.length>1){
@@ -1239,9 +1304,10 @@ function playDMAV(id=30621030,page=2,data=null) {
             video.url = video_url;
             video.artworkImageURL = data.wb_img;
             video.options = {headers:{
-                "User-Agent": ua,
-                "referer": data.wb_full_url
+                "User-Agent": USER_AGENT,
+                "referer": `http://www.bilibili.com/video/av${data.aid}`
             }};
+            video.mp4 = false;
             video.title = `P${part.page}:${part.name} - ${data.wb_desc}`;
             video.description = data.wb_summary;
             videoList.push(video);
@@ -1282,20 +1348,21 @@ function playDMAV(id=30621030,page=2,data=null) {
 }
 
 
-function getAvData(id,page,cd){
+function getAvData(id,page,cb){
+    getBilibiliURL(id,page,cb);
     
-    var APIURL = "https://www.biliplus.com/api/geturl?update=1&av=" + id + "&page=" + page;
-    var url = `https://www.bilibili.com/video/av${id}/?p=${page}`;
+    // var APIURL = "https://www.biliplus.com/api/geturl?update=1&av=" + id + "&page=" + page;
+    // var url = `https://www.bilibili.com/video/av${id}/?p=${page}`;
 
-    console.log('get av data', id, page, cd);
+    // console.log('get av data', id, page, cd);
     
-    ajax.get(APIURL,function (html) {
-       console.log(html);
-        if(html){
-           cd(html);
-        }
+    // ajax.get(APIURL,function (html) {
+    //    console.log(html);
+    //     if(html){
+    //        cd(html);
+    //     }
 
-    });
+    // });
 
 
 }
